0d61efdfeef6d4d5894070e9d180c5440827e864
Andrea Takamiya <takamiya.andrea@gmail.com>
2019-01-08 17:06:16 -0800

    Finalizing the first pangram solution with notes

--- pangram.py
+++ pangram.py
@@ -1,39 +1,40 @@
-# Write a function which determines if a given string is a pangram.
+# Solution 3 - O^n2 Quadratic Time
 
-# Questions:
+# Question:
 # Each time I run the timer, it gives me a different result for the same problem. That makes sense because my computer is probably running different things at different times, but how can I use the timer as an accurate way to compare different solutions to each other?
-
 
 import time
 
 def checkIfPangramSolutionOne(n):
   start = time.time()
 
-# Check to make sure n is a string
+# Check to make sure n is a string - constant time
   if isinstance(n, str) != True:
     return False
 
   removeSpaces = n.replace(' ','')
   toLowerCase = removeSpaces.lower()
+  ## toLowerCase runs a For loop + if statement so O^n2 Quadratic time
   createList = list(toLowerCase)
+  ## Sorted list O(nlogn) logarithmic time
   sortedList = sorted(createList)
   
   fullAlphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-  alphabetByFrequency = ['e','t','a','o','i','n','s','h','r','d','l','u','c','m','f','w','y','p','v','b','g','k','j','q','x','z'];
 
-  for i in xrange(len(fullAlphabet)):
-    for y in xrange(len(sortedList)):
-      if fullAlphabet[i] == sortedList[y]:
-        print(fullAlphabet[i])
-        break
-        
-    print('not pangram')
-    return False
-
-    end = time.time()
-    print(end - start)
-
+  ## Iterating over elements required n steps so O(n) linear time
+  for letter in fullAlphabet:
+    ## If statement is O(n) linear time
+    if not letter in sortedList:
+      end = time.time()
+      print(end - start)
+      print('not a pangram')
+      return False
+  
+  end = time.time()
+  print(end - start)
+  print('It is a pangram!')
+  return True
+  
 checkIfPangramSolutionOne('The Quick Brown Fox Jumps Over the Lazy Dog')
-
-# // Make a note of how you came to that strategy and what the time and space complexity are.
+checkIfPangramSolutionOne('The Brown Fox Jumps Over the Lazy Dog')
 
